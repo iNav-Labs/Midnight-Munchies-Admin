2a48d7297d023c4c6656dd688e94bd6d4a22c511
kevalpanwala27 <kevalpanwala08@gmail.com>
2025-03-18 17:15:09 +0530

    made some last changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
-// import 'package:admin_side/screens/admin_auth_screen.dart';
-// import 'package:admin_side/screens/admin_home_screen.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(options: DefaultFirebaseOptions.web);
-
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       initialRoute: '/login',
-//       routes: {
-//         '/login':
-//             (context) => AuthScreen(
-//               onRegistrationComplete: () {
-//                 Navigator.pushReplacementNamed(context, '/home');
-//               },
-//             ),
-//         '/home': (context) => const HomeScreen(),
-//       },
-//     );
-//   }
-// }
-
-// // vfasdvadfvadfbvsdfbdgabgbadgmblksgjlk13245gte!$1@#@
-
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -127,6 +91,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Admin Midnight Munchies',
       initialRoute: '/login',
       routes: {
         '/login':
